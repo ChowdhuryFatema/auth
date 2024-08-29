81864d4b34421866917a517fe80a196c8dad8cad
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-08-29 16:54:52 +0600

    Added Some Code

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './Routes/authRoute.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -19,6 +21,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();

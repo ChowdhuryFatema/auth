aa2325c0bc55595d3cce1c98c1b1341a36f3e9a4
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-08-29 10:52:28 +0600

    Auth Project Complete

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -21,55 +21,62 @@ const LoginPage = () => {
 			initial={{ opacity: 0, y: 20 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ duration: 0.5 }}
-			className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
+			className='max-w-3xl w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl shadow-xl overflow-hidden grid grid-cols-1 md:grid-cols-2 m-5'
 		>
-			<div className='p-8'>
-				<h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
-					Welcome Back
-				</h2>
+			
 
-				<form onSubmit={handleLogin}>
-					<Input
-						icon={Mail}
-						type='email'
-						placeholder='Email Address'
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-
-					<Input
-						icon={Lock}
-						type='password'
-						placeholder='Password'
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-
-					<div className='flex items-center mb-6'>
-						<Link to='/forgot-password' className='text-sm text-green-400 hover:underline'>
-							Forgot password?
-						</Link>
-					</div>
-					{error && <p className='text-red-500 font-semibold mb-2'>{error}</p>}
-
-					<motion.button
-						whileHover={{ scale: 1.02 }}
-						whileTap={{ scale: 0.98 }}
-						className='w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200'
-						type='submit'
-						disabled={isLoading}
-					>
-						{isLoading ? <Loader className='w-6 h-6 animate-spin  mx-auto' /> : "Login"}
-					</motion.button>
-				</form>
+			<div>
+				<dotlottie-player src="https://lottie.host/2debcff3-9d6d-4898-9e9a-736a3d434f73/AF3rkfn5dt.lottie" background="transparent" speed="1" style={{ width: '100%', height: '100%' }} direction="1" playMode="normal" loop autoplay></dotlottie-player>
 			</div>
-			<div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
-				<p className='text-sm text-gray-400'>
-					Don't have an account?{" "}
-					<Link to='/signup' className='text-green-400 hover:underline'>
-						Sign up
-					</Link>
-				</p>
+			<div className='w-full md:border-t-0 md:border-l border-t border-gray-500 mx-auto'>
+				<div className='p-8'>
+					<h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
+						Welcome Back
+					</h2>
+
+					<form onSubmit={handleLogin}>
+						<Input
+							icon={Mail}
+							type='email'
+							placeholder='Email Address'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+
+						<Input
+							icon={Lock}
+							type='password'
+							placeholder='Password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+
+						<div className='flex items-center mb-6'>
+							<Link to='/forgot-password' className='text-sm text-green-400 hover:underline'>
+								Forgot password?
+							</Link>
+						</div>
+						{error && <p className='text-red-500 font-semibold mb-2'>{error}</p>}
+
+						<motion.button
+							whileHover={{ scale: 1.02 }}
+							whileTap={{ scale: 0.98 }}
+							className='w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200'
+							type='submit'
+							disabled={isLoading}
+						>
+							{isLoading ? <Loader className='w-6 h-6 animate-spin  mx-auto' /> : "Login"}
+						</motion.button>
+					</form>
+				</div>
+				<div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
+					<p className='text-sm text-gray-400'>
+						Don't have an account?{" "}
+						<Link to='/signup' className='text-green-400 hover:underline'>
+							Sign up
+						</Link>
+					</p>
+				</div>
 			</div>
 		</motion.div>
 	);
